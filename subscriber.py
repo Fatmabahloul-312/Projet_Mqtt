70dd3c1275c4c707da186f69021dc10cf42c3b82
fatmabahloul312 <bahloulf312@gmail.com>
2023-01-20 13:31:59 +0100

    first commit

---
+++ subscriber.py
@@ -0,0 +1,30 @@
+from paho.mqtt import client as mqtt_client
+from faker import Faker
+import time
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "measure/tempurature"
+fake = Faker()
+
+
+
+# Create a new client instance
+client = mqtt_client.Client()
+fake = Faker()
+
+
+# Connect to the broker
+client.connect(broker, port, 60)
+
+# Publish a message
+client.subscribe(topic)
+
+while True:
+    temp = fake.random_int(min=-30, max=60)
+    client.publish(topic, temp)
+    print(f"Published new temp measurement: {temp}")
+    time.sleep(4)
+
+# Disconnect from the broker
+client.disconnect()
